d104544f201d721f03eed223d4e8ae543a34eefb
marinaotok <marinaotokfaculdade@gmail.com>
2021-11-23 16:07:42 -0300

    FRONT

---
+++ front-gibis/src/components/pages/Cadastros.js
@@ -0,0 +1,4 @@
+function Cadastros(){
+    return <h1>Cadastros</h1>
+}
+export default Cadastros
---
+++ front-gibis/src/components/pages/Financeiro.js
@@ -0,0 +1,4 @@
+function Financeiro(){
+    return <h1>Financeiro</h1>
+}
+export default Financeiro
