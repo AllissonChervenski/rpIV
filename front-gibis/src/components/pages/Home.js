05c4cc909f724ebf4b872637895f657b5714620b
marinaotok <marinaotokfaculdade@gmail.com>
2021-11-30 16:43:43 -0300

    Construção Home

--- front-gibis/src/components/pages/Home.js
+++ front-gibis/src/components/pages/Home.js
@@ -1,4 +1,19 @@
-function Home(){
-    return <h1>Home</h1>
+import styles from './Home.module.css'
+import savings from '../../img/savings.png'
+
+import LinkButton from '../layout/LinkButton'
+
+function Home() {
+  return (
+    <section className={styles.home_container}>
+      <h1>
+        Bem-vindo à <span>Hero Gibis!</span>
+      </h1>
+      <p>Site em construção</p>
+      <LinkButton to="/cadastros" text="Teste Botão Cadastros" />
+      <img src={savings} alt="Savings" />
+    </section>
+  )
 }
+
 export default Home
