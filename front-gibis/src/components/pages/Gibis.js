9488a5b7cfd5c89f0d4a31c4d06fc43d53677e16
marinaotok <marinaotokfaculdade@gmail.com>
2021-11-30 20:51:14 -0300

    Conclusão formulário gibis

--- front-gibis/src/components/pages/Gibis.js
+++ front-gibis/src/components/pages/Gibis.js
@@ -21,9 +21,16 @@ function Gibis(){
         <div className={styles.gibis_container}>
             <h1>Cadastrar novo Gibi</h1>
             <p>Insira no formulário abaixo as informações solicitadas:</p>
+<<<<<<< HEAD
             <GibisForm />
 >>>>>>> c1653e8 (Campos de cadastro de gibis)
+<<<<<<< HEAD
 >>>>>>> 18d26fa (Campos de cadastro de gibis)
+=======
+=======
+            <GibisForm btnText="Cadastrar Gibi"/>
+>>>>>>> 75195b7 (Conclusão formulário gibis)
+>>>>>>> ef00674 (Conclusão formulário gibis)
         </div>
     )
 }
