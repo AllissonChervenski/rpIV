26de1a7755f5b5dd47ee307c6da67fc0cff187b2
marinaotok <marinaotokfaculdade@gmail.com>
2021-12-14 00:57:25 -0300

    Ajustes

--- front-gibis/src/components/project/EscritorForm.js
+++ front-gibis/src/components/project/EscritorForm.js
@@ -17,6 +17,7 @@ function EscritorForm({btnText}){
             <Input type="text" text="Email" name="EmailEsc" placeholder="Insiera um email para contato" />
 
 <<<<<<< HEAD
+<<<<<<< HEAD
             <Input type="date" text="Data de Nascimento" name="nascEsc" placeholder="Inserir data de nascimento do escritor" />
 
             <Input type="date" text="Data de Falecimento" name="falecEsc" placeholder="Não preencher caso escritor esteja vivo" />
@@ -26,13 +27,20 @@ function EscritorForm({btnText}){
             <Input type="date" text="Data de Demissão" name="DemissEsc" placeholder="Não preencher caso escritor esteja ativo" />
 =======
             <Input type="number" text="Ano de Nascimento" name="nascEsc" placeholder="Inserir ano de nascimento do escritor" />
+=======
+            <Input type="date" text="Data de Nascimento" name="nascEsc" placeholder="Inserir data de nascimento do escritor" />
+>>>>>>> e45f1fa (Ajustes)
 
-            <Input type="number" text="Ano de Falecimento" name="falecEsc" placeholder="Não preencher caso escritor esteja vivo" />
+            <Input type="date" text="Data de Falecimento" name="falecEsc" placeholder="Não preencher caso escritor esteja vivo" />
             
-            <Input type="number" text="Ano de Contratação" name="contratEsc" placeholder="Inserir ano de contratação do escritor" />
+            <Input type="date" text="Data de Contratação" name="contratEsc" placeholder="Inserir data de contratação do escritor" />
 
+<<<<<<< HEAD
             <Input type="number" text="Ano de Demissão" name="DemissEsc" placeholder="Não preencher caso escritor esteja ativo" />
 >>>>>>> 1fe8be3 (Formulário Escritor - Cadastro)
+=======
+            <Input type="date" text="Data de Demissão" name="DemissEsc" placeholder="Não preencher caso escritor esteja ativo" />
+>>>>>>> e45f1fa (Ajustes)
 
             <Select name="pais_nasc_op" text="Selecione o país de nascimento do autor" />
             
--- front-gibis/src/components/project/GibisForm.js
+++ front-gibis/src/components/project/GibisForm.js
@@ -35,9 +35,9 @@ function GibisForm(){
         <form className={styles.form}>
             <Input type="text" text="Nome do Gibi" name="nomeGibi" placeholder="Insira o nome completo do Gibi" />
 
-            <Input type="number" text="Início das publicações" name="inicioP" placeholder="Insira o ano de início das publicações" />
+            <Input type="date" text="Início das publicações" name="inicioP" placeholder="Insira a data de início das publicações" />
             
-            <Input type="number" text="Fim das publicações" name="fimP" placeholder="Não preencher caso as publicações estejam ativas" />
+            <Input type="date" text="Fim das publicações" name="fimP" placeholder="Não preencher caso as publicações estejam ativas" />
 
 <<<<<<< HEAD
             <div>
