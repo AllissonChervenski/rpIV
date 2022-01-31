3e8e40bc41db12d7f9f16504bb5c4e8f263e321d
AllissonChervenski <31050527+AllissonChervenski@users.noreply.github.com>
2022-01-31 16:37:18 -0300

    Delete novo directory

--- novo/src/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        double area;
-        double raio;
-        area = Math.PI * Math.pow(raio,2);
-
-        System.out.println(area);
-    }
-}
