892ae8c72a796719a7ced117e0b68ae62df226a1
AllissonChervenski <allissonchervenski5@gmail.com>
2022-02-03 07:42:37 -0300

    Revert "teste"
    This reverts commit de86d0d777233aaca8250dfe07fd1de5596c962d.

---
+++ editoragibi/src/main/java/com/editoragibi/editoragibi/EditoragibiApplication.java
@@ -0,0 +1,16 @@
+package com.editoragibi.editoragibi;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EditoragibiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditoragibiApplication.class, args);
+	}
+
+
+}
