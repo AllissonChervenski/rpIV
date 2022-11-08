6f94d99381130b993f1babee87a52869a0d84ae1
Amanda Spolaor <amandaspolaor.aluno@unipampa.edu.br>
2022-11-08 14:17:24 -0300

    visualizar desenhista

--- editoragibi/src/main/java/com/grupo2/editoragibi/Api/Controllers/DesenhistaController.java
+++ editoragibi/src/main/java/com/grupo2/editoragibi/Api/Controllers/DesenhistaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/desenhistas/")
 public class DesenhistaController {
