2fa376eafbd8ece3853d7fa53c803694efd5b8b4
Amanda Spolaor <amandaspolaor.aluno@unipampa.edu.br>
2022-11-08 23:09:01 -0300

    tentativa da cadastro de desenhista

--- front-gibis/src/components/pages/NewDesenhista.js
+++ front-gibis/src/components/pages/NewDesenhista.js
@@ -12,63 +12,43 @@ function NewDesenhista() {
 }
 export default NewDesenhista;
 
-const criarDesenhistaElement = (item) => {
-  const template = document.getElementById('desenhista-template')
-  const desenhistaElement = document.importNode(template.content, true)
-  const itens_desenhista =  desenhistaElement.querySelectorAll('span')
-
-  itens_desenhista[0].innerText = item.nomeDesenhista
-  itens_desenhista[1].innerText = item.paisNascimento
-  itens_desenhista[2].innerText = item.dataNascimento
-  itens_desenhista[3].innerText = item.dataFalecimento
-  itens_desenhista[4].innerText = item.enderero
-  itens_desenhista[5].innerText = item.dataContratacao
-  itens_desenhista[5].innerText = item.dataDemissao
-
-  return desenhistaElement
-
-}
-
-
-const novoDesenhista = async () => {
-  const nomeElement = document.getElementById('nome')
-  const paisNascimentoElement = document.getElementById('paisNascimento')
-  const dataNascimentoElement = document.getElementById(' dataNascimento')
-  const dataFalecimentoElement = document.getElementById('dataFalecimento')
-  const enderecoElement = document.getElementById('enderero')
-  const dataContratacaoElement = document.getElementById('dataContratacao')
-  const dataDemissaoElement = document.getElementById('dataDemissao')
-
-  const desenhista = {
-    nome_desenhista: nomeElement.value,
-    pais_Nascimento: paisNascimentoElement.value,
-    data_nascimento_desenhista: dataNascimentoElement.value,
-    data_falecimento_desenhista: dataFalecimentoElement.value,
-    endereco_desenhista: enderecoElement.value,
-    data_contratacao: dataContratacaoElement.value,
-    data_demissao: dataDemissaoElement.value,
-  }
-
-  const response = await fetch('http://localhost:8080/desenhistas/create', {
-    method: 'POST',
-    haeders: {
-      "Content-Type": 'application/json'
-    },
-    body: JSON.stringify(desenhista)
-  })
-  const dados = await response.json()
-
-  const containerDesenhistasElement = document.getElementById('container-desenhistas')
-
-  const desenhistaElement = criarDesenhistaElement(dados)
-
-  containerDesenhistasElement.append(desenhistaElement)
-    
-  }
 
 window.onload = function() {
-    const addDesenhista = document.getElementById('addDesenhista')
-    addDesenhista.onclick = novoDesenhista
-        console.log('ok')
-};
+  const form = document.querySelector('#formulario');
+  form.addEventListener('submit', async function novoDesenhista(){
+    const nomeElement = document.getElementById('nomeDesenhista')
+    const paisNascimentoElement = document.getElementById('paisNascimentoDe')
+    const dataNascimentoElement = document.getElementById(' dataNascimentoDe')
+    const dataFalecimentoElement = document.getElementById('dataFalecimentoDe')
+    const enderecoElement = document.getElementById('enderecoDesenhista')
+    const dataContratacaoElement = document.getElementById('contratacaoDesenhista')
+    const dataDemissaoElement = document.getElementById('demissaoDesenhista')
+    
+    try{
+      const rawResponse = await fetch('http://localhost:8080/desenhistas/create', {
+        method: 'POST',
+        headers:
+          { 'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify ({
+                nomeDesenhista: nomeElement.value,
+                paisNascimento: paisNascimentoElement.value,
+                dataNascimento: dataNascimentoElement.value,
+                dataFalecimento: dataFalecimentoElement.value,
+                enderecoDesenhista: enderecoElement.value,
+                dataContratacao: dataContratacaoElement.value,
+                dataDemissao: dataDemissaoElement.value,
+      
+              }),
+      })
+      
+      alert(rawResponse.json())
+    
+      }catch(err){
+        alert('erro', err)
+      }
 
+  })
+    
+};
--- front-gibis/src/components/project/DesenhistaForm.js
+++ front-gibis/src/components/project/DesenhistaForm.js
@@ -4,27 +4,30 @@ import SubmitButtom from '../form/SubmitButton'
 import styles from './DesenhistaForm.module.css'
 
 
-function DesenhistaForm({ btnText }) {
+function DesenhistaForm() {
     return (
-        <form className={styles.form}>
-            <Input type="text" id="nome" text="Nome do Desenhista" name="nomeDesenhista" placeholder="Insira o nome completo do Desenhista" />
+        <form className={styles.form} id="formulario" method='POST'>
+            <Input type="text" id="nomeDesenhista" text="Nome do Desenhista" name="nomeDesenhista" placeholder="Insira o nome completo do Desenhista" />
 
-            <Input type="text" id="paisNascimento" text="Pais Nacimento" name="paisNascimentoDe" placeholder="País de nascimento do Desenhista" />
+            <Input type="text" id="paisNascimentoDe" text="Pais Nacimento" name="paisNascimentoDe" placeholder="País de nascimento do Desenhista" />
 
-            <Input type="date" id="dataNascimento" text="Data de Nascimento" name="dataNascimentoDe" placeholder="Insira a data de Nascimento" />
+            <Input type="date" id="dataNascimentoDe" text="Data de Nascimento" name="dataNascimentoDe" placeholder="Insira a data de Nascimento" />
 
-            <Input type="date" id="dataFalecimento" text="Data de Falecimento" name="dataFalecimentoDe" placeholder="Insira a data de Falecimento" />
+            <Input type="date" id="dataFalecimentoDe" text="Data de Falecimento" name="dataFalecimentoDe" placeholder="Insira a data de Falecimento" />
 
-            <Input type="text" id="endereco" text="Endereço" name="enderecoDesenhista" placeholder="Insira o endereço do desenhista" />
+            <Input type="text" id="enderecoDesenhista" text="Endereço" name="enderecoDesenhista" placeholder="Insira o endereço do desenhista" />
 
-            <Input type="date" id="dataContratacao" text="Data contratação" name="contratacaoDesenhista" placeholder="Insira a data de contratação" />
+            <Input type="date" id="contratacaoDesenhista" text="Data contratação" name="contratacaoDesenhista" placeholder="Insira a data de contratação" />
 
-            <Input type="date" id="dataDemissao" text="Data demissão" name="demissaoDesenhista" placeholder="Insira a data de demissão (se houver)" />
+            <Input type="date" id="demissaoDesenhista" text="Data demissão" name="demissaoDesenhista" placeholder="Insira a data de demissão (se houver)" />
 
-            <SubmitButtom text={btnText} name="addDesenhista" id="addDesenhista"/>
+            <button type="submit" id="addDesenhista"/>
         </form>
     )
-}
+    }
+
+
+
 
 
 export default DesenhistaForm
