3194824ce0df7ab5f781b12c0da468f2aa498749
Amanda Spolaor <amandaspolaor.aluno@unipampa.edu.br>
2022-12-05 13:58:41 -0300

    ajustes desenhista

--- front-gibis/src/components/pages/Desenhista.js
+++ front-gibis/src/components/pages/Desenhista.js
@@ -1,7 +1,14 @@
 import { React, useState, useEffect } from "react";
-import api from "../../services/api";
-import moment from 'moment';
+import api from "../../../src/services/api.js";
+import axios from 'axios';
+import '../../../src/css/records.css'
+import '../../../src/css/button.css'
+import '../../../src/css/style.css'
+import '../../../src/css/main.css'
 
+<script>
+
+</script>
 export default function Desenhista() {
   const [desenhistas, setDesenhistas] = useState([]);
 
@@ -12,25 +19,57 @@ export default function Desenhista() {
   const getDesenhistas = async () => {
     await api.get("/desenhistas/all").then((response) => {
       setDesenhistas(response.data);
+      console.log(response.data)
     });
   };
 
-  return (
-    <ul>
-      {desenhistas.map((desenhista, index) => (
-        <li key={index}>
-          <h1>{desenhista.nomeDesenhista}</h1>
-          <h1>{desenhista.paisNascimento}</h1>
-          <h1>{moment(desenhista.dataNascimento).format('DD/MM/YYYY')}</h1>
-          <h1>{moment(desenhista.dataFalecimento).format('DD/MM/YYYY')}</h1>
-          <h1>{desenhista.enderecoDesenhista}</h1>
-          <h1>{moment(desenhista.dataContratacao).format('DD/MM/YYYY')}</h1>
-          <h1>{moment(desenhista.dataDemissao).format('DD/MM/YYYY')}</h1>
-          
-          
-        </li>
-      ))}
-    </ul>
-  );
-
+  
+const delDesenhista = (desenlhistaId)=>{
+  // deleteDesenhista(desenhistaId)
+  axios.delete(`http://localhost:8080/desenhistas/${desenlhistaId}`)
+  
 }
+  return (   
+   
+    <table className="records" border="1" >
+      <caption className="caption-title">Desenhista Cadastrado</caption>
+      <thead>
+      <tr >
+              <th className="conteudo">id</th>                      
+              <th className="conteudo">Nome</th>
+              <th className="conteudo">Pais</th>
+              <th className="conteudo">Data de Nascimento</th>
+              <th className="conteudo">Data contratação</th>
+              <th className="conteudo">Data Falecimento</th>
+              <th className="conteudo">Endereço</th>
+              <th className="conteudo">Data demissão</th>
+              <th className="acoes">Ações</th>
+            </tr>
+
+      </thead>
+     
+      {desenhistas.map((desenhista) => (
+        
+           
+            <tr >
+              <td className="conteudo" >{desenhista.desenhistaId}</td>
+              <td className="conteudo">{desenhista.nomeDesenhista}</td>
+              <td className="conteudo">{desenhista.paisNascimento}</td>
+              <td className="conteudo">{desenhista.dataNascimento}</td>
+              <td className="conteudo">{desenhista.dataContratacao}</td>
+              <td className="conteudo">{desenhista.dataFalecimento}</td>
+              <td className="conteudo">{desenhista.enderecoDesenhista}</td>
+              <td className="conteudo" >{desenhista.dataDemissao}</td>
+              <td>
+                <button  className="acoes" type="button" class="button green" id="edit-${desenhistaId}" >Editar</button>
+                <button className="acoes" type="button" class="button red" onClick={() =>delDesenhista(desenhista.desenhistaId)}>Delete</button>
+            </td>
+            </tr>
+            
+        
+        ))}
+        </table>
+       
+    )
+    }
+  
